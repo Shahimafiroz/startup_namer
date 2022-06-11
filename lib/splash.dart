6f822bf66a3d9db1d70f0330f00adca529518486
SHAHIMA FIROZ KHAN <99420269+Shahimafiroz@users.noreply.github.com>
2022-06-12 00:25:21 +0530

    commit2

--- lib/splash.dart
+++ lib/splash.dart
@@ -10,6 +10,65 @@ class Splash extends StatefulWidget {
 class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Stack(
+      children: [
+        Positioned(
+          child: Container(
+            width: screenWidth,
+            height: screenHeight,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/splash3.png"),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
+              ),
+            ),
+          ),
+        ),
+
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            height: screenHeight * 0.40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(20.0),
+              ),
+              gradient: RadialGradient(
+                colors: [
+                  Colors.white.withOpacity(0.20),
+                  Colors.white.withOpacity(0.10),
+                ],
+                radius: 50.0,
+              ),
+            ),
+          ),
+        )
+
+        // Scaffold(
+        //   backgroundColor: Colors.transparent,
+        //   body: Column(
+        //     children: [
+        //       Flexible(
+        //         child: Center(
+        //           child: Text(
+        //             "Crypton",
+        //             style: TextStyle(
+        //               color: Colors.white,
+        //               fontSize: 50,
+        //               fontWeight: FontWeight.bold,
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     ],
+        //   ),
+        // ),
+      ],
+    );
   }
 }
